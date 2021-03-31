7ae5425e9583a1cbed84553b54d0282ad057b130
STEPHANE MBANGOSSOUM <48686867+nadjial@users.noreply.github.com>
2021-03-31 15:32:07 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 ## les variables 
-repertoire="stef/mg"
-namespace=mbango-stephane
-mon-deployment="travis-dep"
-nom="$nom_repo:${TRAVIS_COMMIT::6}"
+repertoire="119220/mg"
+nom="$repertoire"
+namespace=mbang-stephane
+deployment="dep"
+#nom1="$repertoire:${TRAVIS_COMMIT::6}"
+#nom="$repertoire"
 svc="travis-cv"
 public_tcp_port=8080
 private_tcp_port=8000
@@ -29,17 +31,17 @@ gcloud config set project amnay-275801
 gcloud container clusters get-credentials --zone=europe-west4-c my-first-cluster-1
 
 # demarrage sur namespace personel
-kubectl config set-context --current --namespace=mbango-stephane
+kubectl config set-context --current --namespace=mbang-stephane
 
 #  création  du déploiement avec l'image docker (1fois, renvoie already create si existant)
-kubectl create deployment "$nom_deployment" --image=$nom--replicas=3
+kubectl create deployment "$deployment" --image=$nom--replicas=3
 # on crée le service (1fois, renvoie already create si existant)
 kubectl create service clusterip $svc --tcp=8080:8000
 
 # on récupère le deployment en yaml
-kubectl get deployment "$nom_deployment" -o yaml >> dpl.yaml
+kubectl get deployment "$deployment" -o yaml >> dpl.yaml
 # on remplace le nom de l'image par le nom de la nouvelle image (le numéro de ligne change en fonction de la longueur de la dernière config appliquée)
-sed -i -E "s/stef\/mg:[a-z0-9]+/stef\/mg:${TRAVIS_COMMIT::6}/" dpl.yaml
+sed -i -E "s/119220\/mg:[a-z0-9]+/119220\/mg:${TRAVIS_COMMIT::6}/" dpl.yaml
 
 # Update deployment config
 kubectl apply -f dpl.yaml
